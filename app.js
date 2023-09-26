76f36500b5d802f09e7ff8205bc977f7298e42e7
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-26 18:25:55 +0200

    feat: #1 - ajout middleware pour la gestion des erreurs CORS

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
-app.use((req, res) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
+
+app.use(express.json());
+
+// Gestion des erreurs CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
+
 module.exports = app;
