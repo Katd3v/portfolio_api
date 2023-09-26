ddf4c0b4ad409c20a2340923f47be20cd6f84778
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-26 19:25:26 +0200

    feat: #2 - modification du schema de skill

--- models/Skill.js
+++ models/Skill.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const skillSchema = mongoose.Schema({
   title: { type: String, required: true },
   imageUrl: { type: String, required: true },
+  category: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Skills", skillSchema);
