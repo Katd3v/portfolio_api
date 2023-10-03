b088a99571f9196e8db74bb58c47bbd4e5a6abf4
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-10-03 23:11:14 +0200

    refactor: refactor du controller projects

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,20 +1,33 @@
-const { log } = require("console");
-
 const Project = require("../models/Project");
+const Skill = require("../models/Skill");
 
-exports.createProject = (req, res, next) => {
-  const project = new Project({
-    ...req.body,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
-  });
-  project
-    .save()
-    .then(() =>
-      res.status(201).json({ message: "Le projet a bien été enregistré" })
-    )
-    .catch((error) => res.status(400).json({ error }));
+exports.createProject = async (req, res, next) => {
+  //récupérer les noms des langages dans le corps de la requête
+  const languageNames = req.body.languages.split(",");
+
+  try {
+    //rechercher les compétences correspondant dans Skill
+    const foundSkills = await Skill.find({ title: { $in: languageNames } });
+
+    //récupérer les images des compétences trouvées
+    const languageImage = foundSkills.map((skill) => skill.imageUrl);
+
+    //créer le nouveau projet
+    const project = new Project({
+      ...req.body,
+      imageUrl: `${req.protocol}://${req.get("host")}/images/${
+        req.file.filename
+      }`,
+      languages: languageImage,
+    });
+
+    // sauvegarder le projet
+    await project.save();
+
+    return res.status(201).json({ message: "Le projet a bien été enregistré" });
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 };
 
 exports.getAllProjects = (req, res, next) => {
