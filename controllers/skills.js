c25ff4563e4c8e62df208e3c660a77ab90188ac0
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-10-05 17:58:05 +0200

    refactor: envoi des skills sans trier par categorie

--- controllers/skills.js
+++ controllers/skills.js
@@ -16,22 +16,25 @@ exports.createSkill = (req, res, next) => {
 };
 
 exports.getAllSkills = async (req, res, next) => {
-  try {
-    // Récupérer les compétences depuis la base de données par categorie
-    const languages = await Skill.find({ category: "Langages" }).exec();
-    const frameworks = await Skill.find({ category: "Frameworks" }).exec();
-    const tools = await Skill.find({ category: "Outils" }).exec();
+  // try {
+  //   // Récupérer les compétences depuis la base de données par categorie
+  //   const languages = await Skill.find({ category: "Langages" }).exec();
+  //   const frameworks = await Skill.find({ category: "Frameworks" }).exec();
+  //   const tools = await Skill.find({ category: "Outils" }).exec();
 
-    // Créer l'objet de réponse
-    const skills = [languages, frameworks, tools];
+  //   // Créer l'objet de réponse
+  //   const skills = [languages, frameworks, tools];
 
-    // Renvoyer l'objet en tant que réponse JSON
-    res.status(200).json(skills);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error:
-        "Une erreur s'est produite lors de la récupération des compétences.",
-    });
-  }
+  //   // Renvoyer l'objet en tant que réponse JSON
+  //   res.status(200).json(skills);
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).json({
+  //     error:
+  //       "Une erreur s'est produite lors de la récupération des compétences.",
+  //   });
+  // }
+  Skill.find()
+    .then((skill) => res.status(200).json(skill))
+    .catch((error) => res.status(400).json({ error }));
 };
